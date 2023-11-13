b73921fa55bd487baccc67ad3f500041d54a910d
mikoyekel13 <mikoyekel@hilma.tech>
2023-11-13 15:29:37 +0200

    plkayer

--- getTo100/src/components/Game.jsx
+++ getTo100/src/components/Game.jsx
@@ -1,31 +1,36 @@
+import { useState } from "react";
 
-function Game(props) {
-    function randomNumber() {
-        let randomNum = Math.floor(Math.random ()* 100) + 1;
-        props.gameCount(randomNum);
-        // console.log(randomNum);
-        props.buttonOnce(true);
-        return randomNum;
+function Game() {
+  const [gameCount, setGameCount] = useState(0);
+  const [randomClicked, setRandomClicked] = useState(false);
+
+  function randomNumber() {
+    let randomNum = Math.floor(Math.random() * 100);
+    setGameCount(randomNum);
+    setRandomClicked(true);
+    return randomNum;
+  }
+  function plusNumber(number) {
+    setGameCount((prev) => prev + number);
+  }
+  function addNumber(key) {
+    if (key === "*2") {
+      setGameCount((prev) => Math.floor(prev * 2));
+    } else {
+      setGameCount((prev) => Math.floor(prev / 2));
     }
-    function plusNumber(number) {
-        props.gameCount((prev) => prev + number);
-    }
- function addNumber(key){
-    if(key==='*2'){
-        props.gameCount((prev) =>Math.floor( prev*2));
-    }else {
-        props.gameCount((prev) => Math.floor(prev /2));
-    }
- }
-    return (
-        <div>
-            <button onClick={randomNumber} disabled={props.buttonFirst}>Random Number</button>
-            <button onClick={() => addNumber(2)}>/2</button>
-            <button onClick={() => addNumber('*2')}>*2</button>
-            <button onClick={() => plusNumber(+1)}>+1</button>
-            <button onClick={() => plusNumber(-1)}>-1</button>
-            
-        </div>
-    )
+  }
+  return (
+    <div className="gameScreen">
+      <button onClick={randomNumber} disabled={randomClicked}>
+        Random Number
+      </button>
+      <button onClick={() => addNumber(2)}>/2</button>
+      <button onClick={() => addNumber("*2")}>*2</button>
+      <button onClick={() => plusNumber(+1)}>+1</button>
+      <button onClick={() => plusNumber(-1)}>-1</button>
+      <h2>{gameCount}</h2>
+    </div>
+  );
 }
 export default Game;
