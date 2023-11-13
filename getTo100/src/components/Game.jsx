02227ad79d898ad891587a8bc9be43fbc2d3ecb5
Shirshlomo18 <Shirshlomo@hilma.tech>
2023-11-13 16:14:52 +0200

    hahaha

--- getTo100/src/components/Game.jsx
+++ getTo100/src/components/Game.jsx
@@ -2,14 +2,17 @@ import { useState } from "react";
 
 function Game() {
   const [gameCount, setGameCount] = useState(Math.floor(Math.random() * 100));
-
+  const [steps, setSteps] = useState(0);
   function plusNumber(number) {
+    setSteps((prev) => prev + 1);
     setGameCount((prev) => prev + number);
   }
   function addNumber(key) {
     if (key === "*2") {
+      setSteps((prev) => prev + 1);
       setGameCount((prev) => Math.floor(prev * 2));
     } else {
+      setSteps((prev) => prev + 1);
       setGameCount((prev) => Math.floor(prev / 2));
     }
   }
@@ -19,6 +22,7 @@ function Game() {
       <button onClick={() => addNumber("*2")}>*2</button>
       <button onClick={() => plusNumber(+1)}>+1</button>
       <button onClick={() => plusNumber(-1)}>-1</button>
+      <p>Steps: {steps}</p>
       <h2>{gameCount}</h2>
     </div>
   );
